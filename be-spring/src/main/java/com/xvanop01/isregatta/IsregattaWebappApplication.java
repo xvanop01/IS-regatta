25d0fdec4e090ecfae84f50896305d6fd8ea9994
xvanop01 <xvanop01@stud.fit.vutbr.cz>
2023-04-19 14:44:05 +0200

    ISR-01 BE Spring project init

---
+++ be-spring/src/main/java/com/xvanop01/isregatta/IsregattaWebappApplication.java
@@ -0,0 +1,13 @@
+package com.xvanop01.isregatta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsregattaWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsregattaWebappApplication.class, args);
+	}
+
+}
---
+++ be-spring/src/test/java/com/xvanop01/isregatta/IsregattaWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xvanop01.isregatta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsregattaWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
