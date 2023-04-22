feed4c5286e7d13fc49aa239c4a3fad4173cfda2
xvanop01 <xvanop01@stud.fit.vutbr.cz>
2023-04-22 23:41:41 +0200

    ISR-02 Login skeleton

---
+++ rest-api/target/generated-sources/openapi/swagger/com/xvanop01/isregatta/api/dto/UserDetailDto.java
@@ -0,0 +1,80 @@
+package com.xvanop01.isregatta.api.dto;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.openapitools.jackson.nullable.JsonNullable;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * UserDetailDto
+ */
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-04-21T14:25:44.435262222+02:00[Europe/Budapest]")
+
+public class UserDetailDto   {
+  @JsonProperty("name")
+  private String name;
+
+  public UserDetailDto name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * Get name
+   * @return name
+  */
+  @ApiModelProperty(value = "")
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    UserDetailDto userDetailDto = (UserDetailDto) o;
+    return Objects.equals(this.name, userDetailDto.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class UserDetailDto {\n");
+    
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
