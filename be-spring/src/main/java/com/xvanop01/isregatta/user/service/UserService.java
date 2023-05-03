8e32bd6996ae641bd269a00675f7432930bba275
xvanop01 <xvanop01@stud.fit.vutbr.cz>
2023-05-03 04:42:29 +0200

    ISR-03 Added update roles for admins

--- be-spring/src/main/java/com/xvanop01/isregatta/user/service/UserService.java
+++ be-spring/src/main/java/com/xvanop01/isregatta/user/service/UserService.java
@@ -53,9 +53,7 @@ public class UserService {
             throw new HttpException(Http400ReturnCode.CONFLICT,
                     String.format("User with username '%s' already exists", existingUser.getUsername()));
         }
-        log.info(user.getPassword());
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        log.info(user.getPassword());
         return userPersistanceService.persist(user);
     }
 
