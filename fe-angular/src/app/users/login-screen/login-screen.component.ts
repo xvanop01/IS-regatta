b4295547936a77eef6dbda2582c8a92d2df13c32
xvanop01 <xvanop01@stud.fit.vutbr.cz>
2023-05-08 02:12:33 +0200

    ISR-06 Login refactor to ngForm

--- fe-angular/src/app/users/login-screen/login-screen.component.ts
+++ fe-angular/src/app/users/login-screen/login-screen.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { UsersService } from "../users.service";
 
 @Component({
   selector: 'app-login',
@@ -7,11 +10,22 @@ import { Component, OnInit } from "@angular/core";
 })
 export class LoginScreenComponent implements OnInit {
 
-  constructor() {
+  loginForm = this.formBuilder.group({
+    username: '',
+    password: ''
+  });
+
+  constructor(private formBuilder: FormBuilder,
+              private usersService: UsersService,
+              private snackBar: MatSnackBar) {
 
   }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    let credentials = 'username=' + this.loginForm.controls.username.value + '&password=' + this.loginForm.controls.password.value
+    this.usersService.login(credentials).subscribe();
+  }
 }
