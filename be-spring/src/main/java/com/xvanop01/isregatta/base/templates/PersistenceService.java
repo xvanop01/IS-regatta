7f5b8c62a4cf51030c90b7e28e2ae98444b9d2d5
xvanop01 <xvanop01@stud.fit.vutbr.cz>
2023-05-09 22:55:11 +0200

    ISR-07 Added PersistenceService template

---
+++ be-spring/src/main/java/com/xvanop01/isregatta/base/templates/PersistenceService.java
@@ -0,0 +1,34 @@
+package com.xvanop01.isregatta.base.templates;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@NoRepositoryBean
+@Slf4j
+public class PersistenceService<E, R extends JpaRepository<E, Integer> & JpaSpecificationExecutor<E>> {
+
+    protected final R repository;
+
+    @Transactional
+    public E persist(E entity) {
+        log.info("persist: {}", entity);
+        return repository.save(entity);
+    }
+
+    public E findById(Integer id) {
+        log.info("findById: {}", id);
+        return repository.findById(id).orElse(null);
+    }
+
+    public List<E> findAll() {
+        log.info("findAll");
+        return repository.findAll();
+    }
+}
--- be-spring/src/main/java/com/xvanop01/isregatta/user/service/UserService.java
+++ be-spring/src/main/java/com/xvanop01/isregatta/user/service/UserService.java
@@ -39,7 +39,7 @@ public class UserService {
 
     public List<User> getAllUsers() {
         log.info("getAllUsers");
-        return userPersistenceService.findAllUsers();
+        return userPersistenceService.findAll();
     }
 
     @Transactional(rollbackFor = HttpException.class)
