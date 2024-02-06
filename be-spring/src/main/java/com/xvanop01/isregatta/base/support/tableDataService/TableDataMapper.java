00eab2a3917c9d059db606bef97fc1173b04e969
xvanop01 <xvanop01@stud.fit.vutbr.cz>
2024-02-06 23:16:57 +0100

    ISR-08 TableData sorting

--- be-spring/src/main/java/com/xvanop01/isregatta/base/support/tableDataService/TableDataMapper.java
+++ be-spring/src/main/java/com/xvanop01/isregatta/base/support/tableDataService/TableDataMapper.java
@@ -1,11 +1,24 @@
 package com.xvanop01.isregatta.base.support.tableDataService;
 
+import com.xvanop01.isregatta.api.base.model.SortDto;
+import com.xvanop01.isregatta.api.base.model.TableDataRequestDto;
 import com.xvanop01.isregatta.api.base.model.TableDataResponseDto;
 import org.mapstruct.Mapper;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 @Mapper(componentModel = "spring")
 public abstract class TableDataMapper {
+
+    private static final String DIR_ASC = "ASC";
+
+    public PageRequest map(TableDataRequestDto tableDataRequestDto) {
+        SortDto sortDto = tableDataRequestDto.getSortCriteria();
+        Sort.Direction direction = DIR_ASC.equals(sortDto.getDirection()) ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortDto.getColumn());
+        return PageRequest.of(tableDataRequestDto.getPageNumber(), tableDataRequestDto.getPageSize(), sort);
+    }
 
     public abstract TableDataResponseDto map(TableDataResponse tableDataResponse);
 
--- be-spring/src/main/java/com/xvanop01/isregatta/base/support/tableDataService/TableDataServiceController.java
+++ be-spring/src/main/java/com/xvanop01/isregatta/base/support/tableDataService/TableDataServiceController.java
@@ -6,6 +6,8 @@ import com.xvanop01.isregatta.api.base.model.TableDataResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,8 +25,8 @@ public class TableDataServiceController implements TableDataServiceControllerApi
     public ResponseEntity<TableDataResponseDto> getTableDataByServiceName(String serviceName,
            TableDataRequestDto tableDataRequestDto) {
         // TODO log.info()
-        // TODO pageable
-        Page<?> page = tableDataServiceProvider.getTableData(serviceName, null);
+        PageRequest pageRequest = tableDataMapper.map(tableDataRequestDto);
+        Page<?> page = tableDataServiceProvider.getTableData(serviceName, pageRequest);
         TableDataResponseDto dto = tableDataMapper.map(page);
         return ResponseEntity.ok(dto);
     }
