ced70601b813661e6c2d74c6425d76e1d2add952
Peter Vano <peter.vano@coreteq.sk>
2024-04-02 13:56:10 +0200

    ISR-12 Login page format

--- fe-angular/src/app/users/login-screen/login-screen.component.ts
+++ fe-angular/src/app/users/login-screen/login-screen.component.ts
@@ -1,18 +1,54 @@
-import { Component, OnInit } from "@angular/core";
-import {ActivatedRoute} from "@angular/router";
+import {Component, ElementRef, OnInit, ViewChild} from "@angular/core";
+import {ActivatedRoute, RouterLink} from "@angular/router";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {FormControl, FormGroup, NgForm, ReactiveFormsModule} from "@angular/forms";
+import {UsersService} from "../users.service";
+
+enum VisiblePassword {
+  Hide = 'password',
+  Show = 'text'
+}
+
+enum IconPassword {
+  Hide = 'fa-solid fa-eye-slash',
+  Show = 'fa-solid fa-eye'
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login-screen.component.html',
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    RouterLink
+  ],
   styleUrls: ['./login-screen.component.css']
 })
 export class LoginScreenComponent implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute,
-              private snackBar: MatSnackBar) {
+  @ViewChild('formElement') formElement?: ElementRef;
 
+  public visible: VisiblePassword = VisiblePassword.Hide;
+
+  public icon: IconPassword = IconPassword.Show;
+
+  public fg: FormGroup;
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private snackBar: MatSnackBar,
+              private usersService: UsersService) {
+    this.fg = new FormGroup({
+      username: new FormControl(''),
+      password: new FormControl('')
+    })
   }
 
   ngOnInit(): void {
@@ -24,4 +60,19 @@ export class LoginScreenComponent implements OnInit {
     })
   }
 
+  public onIconClicked(): void {
+    if (this.visible === VisiblePassword.Hide) {
+      this.visible = VisiblePassword.Show;
+      this.icon = IconPassword.Hide;
+    } else {
+      this.visible = VisiblePassword.Hide;
+      this.icon = IconPassword.Show;
+    }
+  }
+
+  public onSubmit(): void {
+    if (this.formElement) {
+      this.formElement.nativeElement.submit();
+    }
+  }
 }
